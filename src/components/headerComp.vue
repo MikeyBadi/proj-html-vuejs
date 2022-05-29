adf39ec124cedc989482b7fedc3a14211dbe265c
MikeyBadi <mihaibadia.1999@gmail.com>
2022-05-29 15:43:27 +0200

    FIRST

---
+++ src/components/headerComp.vue
@@ -0,0 +1,58 @@
+<template>
+  <header>
+<!-- /NAV -->
+    <nav>
+      <ul>
+        <li>
+          <a href=""></a>
+        </li>
+
+        <li>
+          <a href=""></a>
+        </li>
+
+        <li>
+          <a href=""></a>
+        </li>
+
+        <li>
+          <a href=""></a>
+        </li>
+
+        <li>
+          <a href=""></a>
+        </li>
+
+        <li>
+          <a href=""></a>
+        </li>
+
+        <li>
+          <a href=""></a>
+        </li>
+
+        <li>
+          <a href=""></a>
+        </li>
+      </ul>
+    </nav>
+<!-- /NAV -->
+
+<!-- JUMBOTRON -->
+    <div class="jumbotron">
+
+    </div>
+<!-- /JUMBOTRON -->
+    
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
